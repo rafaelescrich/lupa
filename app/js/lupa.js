f2bf894cb9246463c19d7d03554f0cec21be6225
Rafael Escrich <rafaelescrich@gmail.com>
2016-04-14 18:57:20 -0300

    Changed keywords

--- app/js/lupa.js
+++ app/js/lupa.js
@@ -8,7 +8,7 @@ angular.module('lupaApp', [])
                   "id"	: "1",
                       "title"	: "Título 1",
                       "description"	: "Apresenta o episódio da série Um mundo de letras, da TV Escola. Mostra a importância do hábito de leitura e escrita na educação das crianças e as dúvidas dos alunos quanto à ortografia das palavras. Apresenta para educadores e professores que sua ação é fundamental para orientar o aluno na descoberta da nova linguagem",
-                      "keywords": "Métodos e Técnicas de Ensino, Educação infantil, Linguagem escrita"
+                      "keywords": "Química, Física, Ensino Médio"
                 }
           },
           {
